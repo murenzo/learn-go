83d8c041703b03f0f2478de352ede1ba62dac7fc
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-02 11:30:18 +0100

    Multi dimensional slice

--- grouping_data/slice/main.go
+++ grouping_data/slice/main.go
@@ -3,8 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	jb := []string {"James", "Bond", "Martini"}
+	mp := []string {"Miss", "Moneypenny", "Strawberry"}
+
+	xp := [][]string {jb, mp}
+
+	fmt.Println(xp)
 	// Make a slice
-	x := make([]int, 10, 12)
+	// x := make([]int, 10, 12)
+
+	// x[9] = 99
+	// x = append(x, 100, 101, 102)
+	// fmt.Println(x)
+	// fmt.Println(len(x))
+	// fmt.Println(cap(x))
 
 	// Composite Literal
 	// x := []int {65, 66, 67, 68, 69, 70}
@@ -19,13 +31,7 @@ func main() {
 
 	// Deleting from a slice
 	// x = append(x[:5], x[7:]...)
-	x[9] = 99
-	x = append(x, 100, 101, 102)
-	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(cap(x))
-
-
+	
 	// Slicing a slice
 	// fmt.Println(x[1:4])
 
