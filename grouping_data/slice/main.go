b690e3df13dc9e29ba2a566e37a64d3a84c0b8bb
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-02 10:47:25 +0100

    Appended to a slice

--- grouping_data/slice/main.go
+++ grouping_data/slice/main.go
@@ -6,8 +6,19 @@ func main() {
 	// Composite Literal
 	x := []int {65, 66, 67, 68, 69, 70}
 
+	// Appending to a slice
+	// x = append(x, 71, 72, 73, 74, 75, 76)
+
+	// Appending a slice to a slice
+	y := []int {71, 72, 73, 74, 75, 76}
+
+	x = append(x, y...)
+
+	fmt.Println(x)
+
+
 	// Slicing a slice
-	fmt.Println(x[1:4])
+	// fmt.Println(x[1:4])
 
 	// Looping a slice using range
 	// for i, v := range x {
