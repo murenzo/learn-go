7c92d6a10d3c36f39839b793b97fc05007e3f5e9
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-02 11:17:04 +0100

    Make a slice

--- grouping_data/slice/main.go
+++ grouping_data/slice/main.go
@@ -3,21 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	// Make a slice
+	x := make([]int, 10, 12)
+
 	// Composite Literal
-	x := []int {65, 66, 67, 68, 69, 70}
+	// x := []int {65, 66, 67, 68, 69, 70}
 
 	// Appending to a slice
 	// x = append(x, 71, 72, 73, 74, 75, 76)
 
 	// Appending a slice to a slice
-	y := []int {71, 72, 73, 74, 75, 76}
+	// y := []int {71, 72, 73, 74, 75, 76}
 
-	x = append(x, y...)
+	// x = append(x, y...)
 
 	// Deleting from a slice
-	x = append(x[:5], x[7:]...)
-
+	// x = append(x[:5], x[7:]...)
+	x[9] = 99
+	x = append(x, 100, 101, 102)
 	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 
 
 	// Slicing a slice
