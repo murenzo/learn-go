a7db0611808d9a8a17cd88f24455b27df8e9e10d
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-29 22:40:11 +0100

    Implement closure with sample code

--- function/main.go
+++ function/main.go
@@ -3,38 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	n := []int {1,2,3,4,5,6,7,8,9,10}
-	sumOddTotal := odd(sum, n...)
-	fmt.Println(sumOddTotal)
+	a := incrementor()
+	b := incrementor()
+
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(b())
 }
 
-func sum(xi ...int) int {
-	total := 0
-
-	for _, v := range xi {
-		total += v
+func incrementor() func()int {
+	var x int
+	return func()int {
+		x++
+		return x
 	}
-	return total
 }
 
-func even(f func(xi ...int) int, xx ...int) int {
-	var yi []int
-	for _, v := range xx {
-		if v % 2 == 0 {
-			yi = append(yi, v)
-		}
-	}
-
-	return f(yi...)
-}
-
-func odd(f func(n ...int) int, v ...int) int {
-	var oddNumbers []int
-	for _, v := range v {
-		if v % 2 != 0 {
-			oddNumbers = append(oddNumbers, v)
-		}
-	}
-
-	return f(oddNumbers...)
-}
