110e5e0550144c1b5be4973bf4004bc2da6775bf
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-29 21:46:59 +0100

    Implement callback into a function

--- function/main.go
+++ function/main.go
@@ -3,13 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	st := foo()
-	v := st()
-	fmt.Println(v)
+	n := []int {1,2,3,4,5,6,7,8,9,10}
+	sumOddTotal := odd(sum, n...)
+	fmt.Println(sumOddTotal)
 }
 
-func foo() func() int {
-	return func() int {
-		return 33
+func sum(xi ...int) int {
+	total := 0
+
+	for _, v := range xi {
+		total += v
 	}
+	return total
+}
+
+func even(f func(xi ...int) int, xx ...int) int {
+	var yi []int
+	for _, v := range xx {
+		if v % 2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+
+	return f(yi...)
+}
+
+func odd(f func(n ...int) int, v ...int) int {
+	var oddNumbers []int
+	for _, v := range v {
+		if v % 2 != 0 {
+			oddNumbers = append(oddNumbers, v)
+		}
+	}
+
+	return f(oddNumbers...)
 }
