d4bd991d9b4206f707ca4245c4682e10723b2a3f
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-29 20:11:16 +0100

    Implement func expression

--- function/main.go
+++ function/main.go
@@ -2,54 +2,15 @@ package main
 
 import "fmt"
 
-type person struct {
-	fname string
-	lname string
-}
-
-type secretAgent struct {
-	person
-	ltk bool
-}
-
-type human interface {
-	speak()
-}
-
-func (p person) speak() {
-	fmt.Println("My name is", p.fname, p.lname, "says person")
-}
-
-func (s secretAgent) speak() {
-	fmt.Println("My name is", s.fname, s.lname, "says secret agent")
-}
-
-func bar(h human) {
-	switch h.(type) {
-	case person:
-		fmt.Println("Human was passed into bar", h.(person).fname)
-	case secretAgent:
-		fmt.Println("Secret Agent was passed into bar", h.(secretAgent).fname)
-	default:
-	fmt.Println("I was passed into bar", h)
-	}
-	
-}
-
 func main() {
-	sa1 := secretAgent {
-		person: person{
-			fname: "Banjoko",
-			lname: "Abdulazeez",
-		},
-		ltk: true,
+	f := func() {
+		fmt.Println("Hello from function expression")
 	}
+	f()
 
-	p1 := person {
-		fname: "Raji-Banjoko",
-		lname: "Sabith",
+	g := func(x int) {
+		fmt.Println("My birth year is:", x)
 	}
-
-	bar(sa1)
-	bar(p1)
+	g(1987)
+	
 }
