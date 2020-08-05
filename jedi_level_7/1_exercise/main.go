b02063cbd35ff271a7be575ec18d745e62b3b02f
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-08-05 23:15:44 +0100

    Complete all exercises in jedi level 7

---
+++ jedi_level_7/1_exercise/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 33
+	fmt.Println(&x)
+}
---
+++ jedi_level_7/2_exercise/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	surname string
+}
+
+func changeMe(p *person) {
+	p.first = "Seun"
+}
+
+func main() {
+	p1 := person {
+		first: "Azeez",
+		surname: "Banjoko",
+	}
+	
+	changeMe(&p1)
+	fmt.Println(p1)
+}
