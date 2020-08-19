84441feb193ff84d04f49b02884a30702326e94a
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-08-19 20:37:00 +0100

    Implement json unmarshal method

--- application/main.go
+++ application/main.go
@@ -1,36 +1,34 @@
 package main
 
 import (
-	"fmt" 
+	"fmt"
 	"encoding/json"
 )
 
 type person struct {
-	Firstname string
-	Lastname  string
-	Age       int
+	Firstname string `json:"Firstname"`
+	Lastname  string `json:"Lastname"`
+	Age       int    `json:"Age"`
 }
 
 func main() {
-	p1 := person{
-		Firstname: "Banjoko",
-		Lastname:  "Abdulazeez",
-		Age:       33,
-	}
+	s := `[{"Firstname":"Banjoko","Lastname":"Abdulazeez","Age":33},{"Firstname":"Raji","Lastname":"Sabith","Age":34}]`
 
-	p2 := person{
-		Firstname: "Raji",
-		Lastname:  "Sabith",
-		Age:       34,
-	}
+	bs := []byte(s)
 
-	people := []person{p1, p2}
+	// people := []person{}
+	var people []person
 
-	bs, err := json.Marshal(people)
+	err := json.Unmarshal(bs, &people)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(string(bs))
+	fmt.Println(people)
+
+	for i, v := range people {
+		fmt.Println("The person index", i)
+		fmt.Println(v.Firstname, v.Lastname, v.Age)
+	}
 }
